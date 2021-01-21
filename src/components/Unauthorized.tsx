5bb10a251e18703ae192b6b7ba02c914e8a484a1
arthuro555 <arthur.pacaud@hotmail.fr>
2021-01-21 14:45:17 +0100

    Rewamp the Unauthorized component styling

--- src/components/Unauthorized.tsx
+++ src/components/Unauthorized.tsx
@@ -4,6 +4,7 @@ import Auth from "../images/auth.svg";
 import { Link } from "gatsby";
 import Button from "@material-ui/core/Button";
 import SEO from "./SEO";
+import Typography from "@material-ui/core/Typography";
 
 export default () => (
   <div
@@ -17,12 +18,15 @@ export default () => (
       title="Unauthorized"
       description="This page is sadly reserved to logged in users. Log in to access it."
     />
-    <h1 style={{ fontSize: "46px" }}>Unauthorized</h1>
-    <Auth style={{ width: "100%", height: "100%" }} />
-    <h3>
-      It seems you are not allowed to be here. Login or request access by the
-      admins.
-    </h3>
+    <Typography variant="h1" style={{ fontSize: "46px" }}>
+      Unauthorized
+    </Typography>
+    <Auth style={{ width: "100%", height: "100%", maxWidth: "500px" }} />
+    <Typography variant="h3" style={{ fontSize: "26px" }}>
+      It seems you are not allowed to be here.
+      <br />
+      Login or ask an administrator for access.
+    </Typography>
     <Link to="/">
       <Button color="primary">Login</Button>
     </Link>
